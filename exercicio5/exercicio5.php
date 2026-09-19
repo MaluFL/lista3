59630d1fe536c6ff2ef76b95c0001945dcd5a5f2
MaluFL <mluizaf.fatec@gmail.com>
2026-09-19 08:29:56 -0300

    implementação do exercício 5

---
+++ exercicio5/exercicio5.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 5</title>
+</head>
+<body>
+    <h1>Exercício 5 - Raiz Quadrada</h1>
+    <form method="post" action="resposta5.php">
+        <label for="text">Informe o número: </label>
+        <input type="number" id="numero" name="numero" step="any">
+        <p>
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
---
+++ exercicio5/resposta5.php
@@ -0,0 +1,4 @@
+<?php
+    $numero = $_POST["numero"];
+    $raiz = sqrt($numero);
+    echo "A raiz quadrada de $numero é: $raiz";
