2720f996927e5161096b27fb912184290915754d
MaluFL <mluizaf.fatec@gmail.com>
2026-09-19 07:33:20 -0300

    implementação do exercício 1

---
+++ exercicio1/exercicio1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 1</title>
+</head>
+<body>
+    <h1>Exercício 1 - Quantidade Caracteres</h1>
+    <form method="post" action="resposta1.php">
+        <label for="text">Escreva a palavra desejada: </label>
+        <input type="text" id="palavra" name="palavra">
+        <p>
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
---
+++ exercicio1/resposta1.php
@@ -0,0 +1,4 @@
+<?php
+    $palavra = $_POST["palavra"];
+    $qtd = strlen($palavra);
+    echo "<p>Quantidade de caracteres da palavra: $qtd</p>";
